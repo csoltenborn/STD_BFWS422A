22475831c107df9091625d1d470d5cf92c20fa1f
Christian Soltenborn <christian.soltenborn@gmail.com>
2024-06-12 18:57:02 +0200

    one passing and next failing test

--- src/test/java/de/fhdw/std/BankServiceIntergrationTest.java
+++ src/test/java/de/fhdw/std/BankServiceIntergrationTest.java
@@ -49,4 +49,32 @@ public class BankServiceIntergrationTest {
         assertThat(sourceAccount).hasBalance(5);
     }
 
+    @Test
+    public void massWithdraw_savingsAccountWitAvailableAmount_withdrawPerformed() {
+        CheckingAccount targetAccount = new CheckingAccount("1", 0, 0, 0);
+        SavingsAccount sourceAccount = new SavingsAccount("2", 1000, 5);
+        bankDatabase.updateAccount(targetAccount);
+        bankDatabase.updateAccount(sourceAccount);
+
+        List<String> failedWithdrawals = bankServiceUnderTest.massWithdraw(targetAccount.getId(), List.of(sourceAccount.getId()), 10);
+
+        assertThat(failedWithdrawals).isEmpty();
+        assertThat(targetAccount).hasBalance(10);
+        assertThat(sourceAccount).hasBalance(990);
+    }
+
+    @Test
+    public void massWithdraw_checkingAccountWithNegativeBalance_withdrawPerformed() {
+        CheckingAccount targetAccount = new CheckingAccount("1", 0, 0, 0);
+        CheckingAccount sourceAccount = new CheckingAccount("2", -100, 1000, 0);
+        bankDatabase.updateAccount(targetAccount);
+        bankDatabase.updateAccount(sourceAccount);
+
+        List<String> failedWithdrawals = bankServiceUnderTest.massWithdraw(targetAccount.getId(), List.of(sourceAccount.getId()), 10);
+
+        assertThat(failedWithdrawals).isEmpty();
+        assertThat(targetAccount).hasBalance(10);
+        assertThat(sourceAccount).hasBalance(-110);
+    }
+
 }
