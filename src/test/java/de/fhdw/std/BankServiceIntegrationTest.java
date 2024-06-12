9ad166202c71dc0936d2c7b68514580acdc5c718
Christian Soltenborn <christian.soltenborn@gmail.com>
2024-06-12 18:58:52 +0200

    (fixed typo)

--- src/test/java/de/fhdw/std/BankServiceIntegrationTest.java
+++ src/test/java/de/fhdw/std/BankServiceIntegrationTest.java
@@ -9,7 +9,7 @@ import static de.fhdw.std.assertions.BankAccountAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class BankServiceIntergrationTest {
+public class BankServiceIntegrationTest {
 
     private BankDatabase bankDatabase;
 
